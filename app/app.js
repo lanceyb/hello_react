6588b167d692d8ccf430492e117ccabb9d4c793f
lance <lance.zyb@gmail.com>
2015-11-22 15:57:54 +0800

    app.js

--- app/app.js
+++ app/app.js
@@ -1,1 +1,8 @@
-console.log("hello react!");
+import React from "react";
+import ReactDom from "react-dom";
+import Greeting from "./greeting";
+
+React.render(
+  <Greeting name="World"/>,
+    document.body
+);
